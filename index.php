9ace34c77304c7b30bb406ef862611297af05f18
was-dtech <ianf@whangamata.school.nz>
2021-01-25 10:58:18 +1300

    website files demo
    This has been added to test GitHub

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intermediate Website</title>
+</head>
+<body>
+    <header>
+        <figure>
+            <img src="images/lc.jpg" alt="logo" width="200">
+        </figure>
+    </header>
+    
+</body>
+</html>
